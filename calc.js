651f5d05dbdcc8fc7b3badb7e6ad145e5cbee2e4
bilecky <51762310+bilecky@users.noreply.github.com>
2021-08-22 20:58:40 +0200

    calculate function and html

---
+++ calc.js
@@ -0,0 +1,24 @@
+
+function bmiCalc() {
+    let height = parseInt(document
+        .querySelector("#height").value);
+    let weight = parseInt(document
+        .querySelector("#weight").value);
+
+    const results = document.querySelector('#results')
+
+    if (height === "" || isNaN(height))
+        results.innerHTML = "Invalid Height! Please use numbers";
+    else if (weight === "" || isNaN(weight))
+        results.innerHTML = "Invalid Weight! Please use numbers"
+    else {
+
+        const bmi = (weight / (height * height))
+        results.innerHTML = bmi
+
+    }
+}
+
+const button = document.querySelector(".results__btn")
+
+button.addEventListener("click", bmiCalc);
