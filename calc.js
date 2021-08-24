06f9742409533b1755eda204af9749f6e771af6f
bilecky <51762310+bilecky@users.noreply.github.com>
2021-08-24 17:51:36 +0200

    added tofixed(2) to results, and correct values

--- calc.js
+++ calc.js
@@ -1,24 +1,19 @@
-
 function bmiCalc() {
-    let height = parseInt(document
-        .querySelector("#height").value);
-    let weight = parseInt(document
-        .querySelector("#weight").value);
+  let height = parseInt(document.querySelector("#height").value);
+  let weight = parseInt(document.querySelector("#weight").value);
 
-    const results = document.querySelector('#results')
+  const results = document.querySelector("#results");
 
-    if (height === "" || isNaN(height))
-        results.innerHTML = "Invalid Height! Please use numbers";
-    else if (weight === "" || isNaN(weight))
-        results.innerHTML = "Invalid Weight! Please use numbers"
-    else {
-
-        const bmi = (weight / (height * height))
-        results.innerHTML = bmi
-
-    }
+  if (height === "" || isNaN(height))
+    results.innerHTML = "Invalid Height! Please use numbers";
+  else if (weight === "" || isNaN(weight))
+    results.innerHTML = "Invalid Weight! Please use numbers";
+  else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed();
+    results.innerHTML = bmi;
+  }
 }
 
-const button = document.querySelector(".results__btn")
+const button = document.querySelector(".results__btn");
 
 button.addEventListener("click", bmiCalc);
